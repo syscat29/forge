e79c9d06e23b1107025412cf7042bbd755883038
Syscat <60985614+syscat29@users.noreply.github.com>
2025-10-17 20:25:07 -0400

    delete docs / web apps and update hono to run on nodejs

--- apps/server/README.md
+++ apps/server/README.md
@@ -1,28 +1,8 @@
-Prerequisites:
-
-- [Vercel CLI](https://vercel.com/docs/cli) installed globally
-
-To develop locally:
-
 ```
 npm install
-vc dev
+npm run dev
 ```
 
 ```
 open http://localhost:3000
-```
-
-To build locally:
-
-```
-npm install
-vc build
-```
-
-To deploy:
-
-```
-npm install
-vc deploy
 ```
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,14 +1,18 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
-
 app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
+  return c.text('Hello Hono!')
 })
 
-export default app
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  },
+)
